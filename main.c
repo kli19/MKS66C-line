4515e11dcecefd493ae861b57843e4e336f75b71
Karen Li <kli19@stuy.edu>
2019-02-10 23:37:56 -0500

    better demo

--- main.c
+++ main.c
@@ -1,30 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "ml6.h"
 #include "display.h"
 #include "draw.h"
 
 int main() {
+  time_t currentTime = time(NULL);
+  srand(currentTime);
 
   screen s;
   color c;
 
-  c.red = 0;
-  c.green = MAX_COLOR;
-  c.blue = 0;
-
   clear_screen(s);
+
+  int centerX = XRES/2;
+  int centerY = YRES/2;
+
+  c.red = 255;
+  c.green = 0;
+  c.blue = 0;
   
-  for (int i = 0; i <= XRES; i+=50){
-    draw_line(i, 0, XRES - i, YRES, s, c);
-    draw_line(0, i, XRES, YRES - i, s, c);
+  for (int x=0, y=0;x<= XRES && y<+ YRES; x+=20, y+=20){
+    draw_line(centerX+x, centerY+y, centerX+y, centerY-x, s, c);
+    draw_line(centerX+y, centerY-x, centerX-x, centerY-y, s, c);
+    draw_line(centerX-x, centerY-y, centerX-y, centerY+x, s, c);
+    draw_line(centerX-y, centerY+x, centerX+x, centerY+y, s, c);
+  }
+
+  c.red = 0;
+  c.green = 255;
+  c.blue = 0;
+  
+  for (int x=0, y=0;x<= XRES && y<+ YRES; x+=10, y+=20){
+    draw_line(centerX+x, centerY+y, centerX+y, centerY-x, s, c);
+    draw_line(centerX+y, centerY-x, centerX-x, centerY-y, s, c);
+    draw_line(centerX-x, centerY-y, centerX-y, centerY+x, s, c);
+    draw_line(centerX-y, centerY+x, centerX+x, centerY+y, s, c);
+  }
+
+  c.red = 0;
+  c.green = 0;
+  c.blue = 255;
+  
+  for (int x=0, y=0;x<= XRES && y<+ YRES; x+=20, y+=10){
+    draw_line(centerX+x, centerY+y, centerX+y, centerY-x, s, c);
+    draw_line(centerX+y, centerY-x, centerX-x, centerY-y, s, c);
+    draw_line(centerX-x, centerY-y, centerX-y, centerY+x, s, c);
+    draw_line(centerX-y, centerY+x, centerX+x, centerY+y, s, c);
   }
   
-
-  int centerX = XRES / 2;
-  int centerY = YRES / 2;
 
   display(s);
   save_extension(s, "lines.png");
